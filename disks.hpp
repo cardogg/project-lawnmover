10e79e409710983342268501729cf46c154596d7
cardogg <rdgonzalez@protonmail.com>
2022-10-09 01:40:15 -0700

    Algorithm implemented though not optimized

--- disks.hpp
+++ disks.hpp
@@ -113,7 +113,7 @@ public:
   // the left (low indices) and all dark disks on the right (high indices).
   bool is_sorted() const {
       //Loops through total list O(n)
-      for (int i = 0; i < total_count() / 2; i++)
+      for (size_t i = 0; i < total_count() / 2; i++)
       {
         //Next we loop through list and see if the half is sorted.
         //Essentially, loop through first half of list and if theres any darks, it is not sorted
@@ -161,12 +161,12 @@ sorted_disks sort_alternate(const disk_state& before) {
   //int state; We can make changes on before
   disk_state after = before;
   //For loop encompasses all light disks -> We can effectively move all of one disk - O(n)
-  for (int i = 0; i < after.light_count(); i++)
+  for (size_t i = 0; i < after.light_count(); i++)
   {
     //For ever light in total until end - O(n)
-    for (int j = i; j < after.total_count() - 1; j++)
+    for (size_t j = i; j < after.total_count() - 1; j++)
     {
-      //swap if 
+      //swap if color is 1,0 so 110 -> 101
       if (after.get(j) > after.get(j + 1))
       {
         after.swap(j);
@@ -182,6 +182,23 @@ sorted_disks sort_alternate(const disk_state& before) {
 // Algorithm that sorts disks using the lawnmower algorithm.
 sorted_disks sort_lawnmower(const disk_state& before) {
   int numOfSwap = 0;
+  disk_state after = before;
+
+  //
+  for (size_t i = 0; i < after.total_count() - 1; i++)
+  {
+    //For ever light in total until end - O(n)
+    for (size_t j = i; j < after.total_count() - 1; j++)
+    {
       
+      if (after.get(j) > after.get(j + 1))
+      {
+        after.swap(j);
+        numOfSwap++;
+      }
+    } 
+  }
+  std::cout << numOfSwap << std::endl;
+  std::cout << after.is_sorted() << std::endl;
   return sorted_disks(disk_state(before), numOfSwap);
 }
